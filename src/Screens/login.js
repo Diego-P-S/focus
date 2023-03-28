4408dd913f3c7b578499ffe49a186ade17b5a7bb
Diego <diego@wevolt.com.au>
2023-03-28 09:36:16 +0800

    up

--- src/Screens/login.js
+++ src/Screens/login.js
@@ -6,14 +6,10 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
+import {TextInput,Text,
+  TouchableOpacity
 } from "react-native";
+import { Container, InputView, Image, ForgotButton, LoginBtn} from './styles';
 
 
 
@@ -28,8 +24,6 @@ const navigation = useNavigation();
           const jsonValue = await AsyncStorage.getItem('@UserData');
           const userData = jsonValue != null ? JSON.parse(jsonValue) : null;
 
-
-          
           if (email === userData.email && password === userData.password) {
               alert('Login realizado com sucesso');
               console.log(userData);
@@ -44,72 +38,31 @@ const navigation = useNavigation();
       }
   }
   return (
-    <View style={styles.container}>
-      <Image resizeMode="contain" style={styles.image} source={require("../../assets/focus.png")} /> 
+    <Container>
+      <Image resizeMode="contain"  source={require("../../assets/focus.png")} /> 
       <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        /> 
-      </View> 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
+
+      <InputView 
+         placeholder="Email"
+         placeholderTextColor="#003f5c"
+         onChangeText={(email) => setEmail(email)}
+      /> 
+    
+      <InputView
           placeholder="Password"
           secureTextEntry={true}
           placeholderTextColor="#003f5c"
           onChangeText={(password) => setPassword(password)}
-        /> 
-      </View> 
+      /> 
+
       <TouchableOpacity onPress={() => navigation.navigate('CreateUser')}>
-        <Text style={styles.forgot_button}>Register</Text> 
+        <ForgotButton>Register</ForgotButton> 
       </TouchableOpacity> 
-      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin} >
-        <Text style={styles.loginText}>LOGIN</Text> 
-      </TouchableOpacity> 
-    </View> 
+
+      <LoginBtn onPress={handleLogin} >
+        <Text>LOGIN</Text> 
+      </LoginBtn> 
+    </Container> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    width: "100%",
-    height: 100,
-    marginBottom: 40,
-  },
-  inputView: {
-    justifyContent:"center",
-    borderColor: "#98d9b6",
-    borderWidth:2,
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  TextInput: {
-   color:"#"
-  },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
-  loginBtn: {
-    width: "70%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#e47b88",
-  },
-});
--- src/Screens/styles.js
+++ src/Screens/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import styled from "styled-components/native";
 
 export const styles = StyleSheet.create({
     Container: {
@@ -40,3 +41,46 @@ export const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
 })
+
+
+
+export const Container = styled.View`
+flex: 1;
+background-color: #fff;
+align-items: center;
+justify-content: center;
+`;
+
+export const Image = styled.Image`
+width: 100%;
+max-height:100px;
+margin-bottom: 40px;
+`;
+
+export const InputView = styled.TextInput`
+justify-content: center;
+border-color: #98d9b6;
+border-width:2px;
+border-radius: 30px;
+width: 70%;
+height: 45px;
+ margin-bottom: 20px;
+align-items: center;
+text-align: center;
+`;
+
+export const ForgotButton = styled.Text`
+height: 30px;
+margin-bottom: 30px;
+`;
+ 
+export const LoginBtn = styled.TouchableOpacity`
+width: 70%;
+border-radius: 25px;
+height: 50px;
+align-items: center;
+justify-content: center;
+margin-top: 40px;
+background-color: #e47b88;
+
+`;
